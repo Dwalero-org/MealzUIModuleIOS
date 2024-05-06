908242121a08f37d8481243352545973ffee723e
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-06 11:51:38 +0200

    update release

---
+++ MealzUIModuleIOS.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |spec|
+    spec.name              = 'MealzUIModuleIOS'
+    spec.version           = '1.0.0'
+    spec.summary           = 'Miam Core for iOS'
+    spec.homepage          = 'https://www.miam.tech'
+       spec.description           = <<-DESC
+       Miam Core SDK for iOS.
+       DESC
+    spec.author            = { 'Vincent Kergonna' => 'it@miam.tech' }
+    spec.license           = { :type => 'GPLv3', :file => 'LICENSE' }
+    spec.swift_versions = "5.8"
+    spec.platform          = :ios, "12.0"
+    spec.source            = { :http => 'https://github.com/miamtech/MealzUIModuleIOS/releases/download/1.0.0/mealzcore.zip' }
+    spec.ios.vendored_frameworks = 'MealzUIModuleIOS/MealzUIModuleIOS.xcframework'
+end
---
+++ Package.swift
@@ -0,0 +1,78 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import Foundation
+
+let configurationMode = ProcessInfo.processInfo.environment["CONFIGURATION_MODE"] ?? "dev"
+
+let package = Package(
+    name: "MealzUIModuleIOS",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12),
+      ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MealzUIModuleIOS",
+            type: .dynamic,
+            targets: ["MealzUIModuleIOS"]),
+    ],
+    dependencies: {
+        var dependencies: [Package.Dependency] = []
+        
+        if configurationMode == "dev" {
+            dependencies.append(contentsOf: [
+                .package(path: "../MealzCore"),
+                .package(path: "../MealzIOSFramework")
+            ]
+            )
+        } else if configurationMode == "devWithSPM" {
+            dependencies.append(contentsOf: [
+                .package(path: "../MealzCore"),
+                .package(path: "../MealzIOSFrameworkSPM")
+            ]
+            )
+        } else {
+            dependencies.append(contentsOf: [
+                .package(url: "https://github.com/miamtech/releaseMealz", from: "1.0.0-beta3"),
+                .package(url: "https://github.com/miamtech/MealzIOSFrameworkSPM", exact: "1.0.0-beta4")
+            ]
+            )
+        }
+        return dependencies
+    }(),
+    targets: [
+        .target(
+            name: "MealzUIModuleIOS",
+            dependencies: {
+                var dependencies: [Target.Dependency] = []
+                if configurationMode == "dev" {
+                    dependencies.append(contentsOf: [
+                        .product(name: "MealzCore", package: "MealzCore"),
+                        .product(name: "MealzIOSFramework", package: "MealzIOSFramework")
+                    ]
+                    )
+                } else if configurationMode == "devWithSPM" {
+                    dependencies.append(contentsOf: [
+                        .product(name: "MealzCore", package: "MealzCore"),
+                        .product(name: "MealzIOSFrameworkSPM", package: "MealzIOSFrameworkSPM")
+                    ]
+                    )
+                } else {
+                    dependencies.append(contentsOf: [
+                        .product(name: "MealzCore", package: "releaseMealz"),
+                        .product(name: "MealzIOSFrameworkSPM", package: "MealzIOSFrameworkSPM")
+                    ]
+                    )
+                }
+                return dependencies
+            }(),
+            resources: [
+                .copy("PrivacyInfo.xcprivacy"),
+                .process("Resources"),
+            ]
+        ),
+    ]
+)
